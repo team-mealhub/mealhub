578ac916cf5087606c33379d3075dee94e8631b4
rnignon <rnignon@naver.com>
2025-09-29 22:40:34 +0900

    feat: createdBy, updatedBy 값 주입을 위한 AuditorAware 구현

---
+++ src/main/java/com/mealhub/backend/global/infrastructure/config/entity/AuditorAwareImpl.java
@@ -0,0 +1,25 @@
+package com.mealhub.backend.global.infrastructure.config.entity;
+
+import com.mealhub.backend.global.infrastructure.config.security.UserDetailsImpl;
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+
+import java.util.Optional;
+
+@Component
+public class AuditorAwareImpl implements AuditorAware<Long> {
+    @Override
+    public Optional<Long> getCurrentAuditor() {
+        Long id = null;
+
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+        if (authentication != null && authentication.getPrincipal() instanceof UserDetailsImpl userDetails) {
+            id = userDetails.getUser().getId();
+        }
+
+        return Optional.ofNullable(id);
+    }
+}
