cc8180c8e72ae128590c00d5560d6fabdb4603fe
rnignon <rnignon@naver.com>
2025-09-29 21:59:33 +0900

    feat: Base Entity 구현

---
+++ src/main/java/com/mealhub/backend/global/domain/entity/BaseAuditEntity.java
@@ -0,0 +1,25 @@
+package com.mealhub.backend.global.domain.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Data;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+@Data
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public abstract class BaseAuditEntity extends BaseTimeEntity {
+
+    @CreatedBy
+    @Column(updatable = false)
+    private Long createdBy;
+
+    @LastModifiedBy
+    @Column(insertable = false)
+    private Long updatedBy;
+
+    private Long deletedBy;
+}
