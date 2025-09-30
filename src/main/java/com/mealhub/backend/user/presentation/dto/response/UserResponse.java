164fe58f73a4715cb24660ff731e2a3ee7e73e58
rnignon <rnignon@naver.com>
2025-09-30 14:51:58 +0900

    feat: UserResponse 작성

---
+++ src/main/java/com/mealhub/backend/user/presentation/dto/response/UserResponse.java
@@ -0,0 +1,24 @@
+package com.mealhub.backend.user.presentation.dto.response;
+
+import com.mealhub.backend.user.domain.entity.User;
+import com.mealhub.backend.user.domain.enums.UserRole;
+import lombok.Data;
+
+@Data
+public class UserResponse {
+    private Long id;
+    private String userId;
+    private String username;
+    private String nickname;
+    private UserRole role;
+    private String phone;
+
+    public UserResponse(User user) {
+        this.id = user.getId();
+        this.userId = user.getUserId();
+        this.username = user.getUsername();
+        this.nickname = user.getNickname();
+        this.role = user.getRole();
+        this.phone = user.getPhone();
+    }
+}
