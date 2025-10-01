b1a454f8ae96e6a913a609177eb12be368584d5b
hellonaeunkim <hellonaeunkim@gmail.com>
2025-10-01 22:27:55 +0900

    feat: 가게 & 가게 카테고리 Dto 생성

---
+++ src/main/java/com/mealhub/backend/restaurant/presentation/dto/request/RestaurantRequest.java
@@ -0,0 +1,10 @@
+package com.mealhub.backend.restaurant.presentation.dto.request;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class RestaurantRequest {
+
+}
