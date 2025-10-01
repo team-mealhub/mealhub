a66e44ab4597eca3649645511794e72759cbed1b
hellonaeunkim <hellonaeunkim@gmail.com>
2025-10-01 22:02:20 +0900

    feat: 가게 & 가게 카테고리 Repository 생성

---
+++ src/main/java/com/mealhub/backend/restaurant/infrastructure/repository/RestaurantCategoryRepository.java
@@ -0,0 +1,10 @@
+package com.mealhub.backend.restaurant.infrastructure.repository;
+
+import com.mealhub.backend.restaurant.domain.entity.RestaurantCategoryEntity;
+import java.util.UUID;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RestaurantCategoryRepository extends
+        JpaRepository<RestaurantCategoryEntity, UUID> {
+
+}
