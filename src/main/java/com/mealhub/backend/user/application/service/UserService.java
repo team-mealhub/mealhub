1efe721ed075ad797972fd9c7edc492e47f1e12c
rnignon <86004439+rnignon@users.noreply.github.com>
2025-10-02 15:47:40 +0900

    [#10] 유저 도메인 기본 구조 구현 (#17)
    * feat: UserDetailsImpl 값으로부터 User 객체를 반환하는 메서드 추가
    
    * feat: UserUpdateRequest 작성
    
    * feat: UserResponse 작성
    
    * feat: UserUpdateRequest 형태의 값을 받아 User 내 값을 수정하는 메서드 추가
    
    * feat: deletedAt이 null이 아닌 유저를 ID로 조회하는 쿼리 메서드 추가
    
    * feat: 유저 조회·수정·삭제 기능을 포함한 UserService 구현
    
    * feat: 유저 조회·수정·삭제 기능을 포함한 UserController 구현
    
    * refactor: Request, Response DTO의 어노테이션 수정
    
    * fix: 유저 조회의 Transactional 어노테이션 누락 수정

---
+++ src/main/java/com/mealhub/backend/user/application/service/UserService.java
@@ -0,0 +1,50 @@
+package com.mealhub.backend.user.application.service;
+
+import com.mealhub.backend.user.domain.entity.User;
+import com.mealhub.backend.user.infrastructure.repository.UserRepository;
+import com.mealhub.backend.user.presentation.dto.request.UserUpdateRequest;
+import com.mealhub.backend.user.presentation.dto.response.UserResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDateTime;
+
+@Service
+@RequiredArgsConstructor
+public class UserService {
+
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    @Transactional(readOnly = true)
+    public UserResponse getUser(Long id) {
+        User user = getUserById(id);
+        return new UserResponse(user);
+    }
+
+    @Transactional
+    public UserResponse updateUser(Long id, UserUpdateRequest request) {
+        User user = getUserById(id);
+        String encodedPassword = passwordEncoder.encode(request.getPassword());
+
+        user.updateUser(request, encodedPassword);
+        return new UserResponse(user);
+    }
+
+    @Transactional
+    public UserResponse deleteUser(Long id) {
+        User user = getUserById(id);
+
+        user.setDeletedAt(LocalDateTime.now());
+        user.setDeletedBy(id);
+
+        return new UserResponse(user);
+    }
+
+    private User getUserById(Long id) {
+        return userRepository.findByIdAndDeletedAtIsNull(id)
+                .orElseThrow(() -> new RuntimeException("Not Found User Temporary message"));
+    }
+}
