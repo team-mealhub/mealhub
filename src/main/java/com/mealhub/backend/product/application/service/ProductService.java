5f71cdf9c4064236e4d023a565228b7a6c78df95
jake8771 <jake525@naver.com>
2025-10-02 12:52:56 +0900

    [#4] 상품 도메인 기본 구조 구현 (#18)
    * feat: 음식(product) DTO , Controlleer,Repository,Service 기본 구조 틀 작성 완료 (진행예정일 상황 : 레포지토리 및 컨트롤러 구현 예정)
    
    * repository,controller,entity 기본 구조 구현 완료 및 수정
    
    * 일부 파일 삭제 및 기본 구조 틀 완료
    
    ---------
    
    Co-authored-by: rnignon <rnignon@naver.com>

---
+++ src/main/java/com/mealhub/backend/product/application/service/ProductService.java
@@ -0,0 +1,4 @@
+package com.mealhub.backend.product.application.service;
+
+public class ProductService {
+}
---
+++ src/main/java/com/mealhub/backend/product/presentation/dto/request/ProductRequest.java
@@ -0,0 +1,17 @@
+package com.mealhub.backend.product.presentation.dto.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class ProductRequest {
+    private UUID rId; // UUID (문자열로 받아서 변환)
+    private String name;
+    private String description;
+    private Long price;
+}
