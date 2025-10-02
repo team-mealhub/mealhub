b9c6dc82bbde5495180cbbe64226ba7b38e1a6c2
rnignon <rnignon@naver.com>
2025-10-02 11:16:39 +0900

    fix: Repository 내 잘못 기입된 사항 수정

--- src/main/java/com/mealhub/backend/cart/infrastructure/repository/CartItemRepository.java
+++ src/main/java/com/mealhub/backend/cart/infrastructure/repository/CartItemRepository.java
@@ -1,8 +1,11 @@
 package com.mealhub.backend.cart.infrastructure.repository;
 
+import com.mealhub.backend.cart.domain.entity.CartItem;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
 
-public interface CartItemRepository extends JpaRepository<CartItemRepository, UUID> {
+@Repository
+public interface CartItemRepository extends JpaRepository<CartItem, UUID> {
 }
