2ee2963f3cf4c5e3bbd86d0cad3a1863cdfdb217
rnignon <rnignon@naver.com>
2025-10-10 15:19:29 +0900

    fix: CartItem 조회 메서드에 status 추가

--- src/main/java/com/mealhub/backend/cart/infrastructure/repository/CartItemRepository.java
+++ src/main/java/com/mealhub/backend/cart/infrastructure/repository/CartItemRepository.java
@@ -1,6 +1,7 @@
 package com.mealhub.backend.cart.infrastructure.repository;
 
 import com.mealhub.backend.cart.domain.entity.CartItem;
+import com.mealhub.backend.cart.domain.enums.CartItemStatus;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -10,5 +11,5 @@ import java.util.UUID;
 
 @Repository
 public interface CartItemRepository extends JpaRepository<CartItem, UUID> {
-    Page<CartItem> findByUserIdAndBuyingIsFalseAndDeletedAtIsNull(Long userId, Pageable pageable);
+    Page<CartItem> findByUserIdAndStatusAndBuyingIsFalseAndDeletedAtIsNull(Long userId, CartItemStatus status, Pageable pageable);
 }
