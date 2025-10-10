d634280825638c539a8294778502e69a4e7ec060
rnignon <rnignon@naver.com>
2025-10-10 16:17:29 +0900

    feat: AuthorizationDeniedException에 대한 예외 처리 추가

--- src/main/java/com/mealhub/backend/global/presentation/advice/CommonControllerAdvice.java
+++ src/main/java/com/mealhub/backend/global/presentation/advice/CommonControllerAdvice.java
@@ -1,9 +1,12 @@
 package com.mealhub.backend.global.presentation.advice;
 
+import com.mealhub.backend.global.domain.application.libs.MessageUtils;
 import com.mealhub.backend.global.domain.exception.CommonException;
 import com.mealhub.backend.global.presentation.dto.ErrorResponse;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authorization.AuthorizationDeniedException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -11,7 +14,10 @@ import java.util.List;
 import java.util.Map;
 
 @RestControllerAdvice("com.mealhub.backend")
+@RequiredArgsConstructor
 public class CommonControllerAdvice {
+
+    private final MessageUtils messageUtils;
 
     @ExceptionHandler(Exception.class)
     public ResponseEntity<ErrorResponse> errorHandler(Exception e) {
@@ -28,5 +34,12 @@ public class CommonControllerAdvice {
         return ResponseEntity
                 .status(status)
                 .body(new ErrorResponse(status, message));
+    }
+
+    @ExceptionHandler(AuthorizationDeniedException.class)
+    public ResponseEntity<ErrorResponse> handleAuthorizationDenied(AuthorizationDeniedException e) {
+        return ResponseEntity
+                .status(HttpStatus.FORBIDDEN)
+                .body(new ErrorResponse(HttpStatus.FORBIDDEN, messageUtils.getMessage("Forbidden")));
     }
 }
