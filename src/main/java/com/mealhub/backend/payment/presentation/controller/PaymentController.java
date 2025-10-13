643c8ff4ff76caa08f80a64e82e5d1085b958dc3
rnignon <rnignon@naver.com>
2025-10-13 17:02:11 +0900

    style: 불필요한 import 문 제거

--- src/main/java/com/mealhub/backend/payment/presentation/controller/PaymentController.java
+++ src/main/java/com/mealhub/backend/payment/presentation/controller/PaymentController.java
@@ -1,15 +1,12 @@
 package com.mealhub.backend.payment.presentation.controller;
 
 import com.mealhub.backend.payment.application.service.PaymentService;
-import com.mealhub.backend.payment.domain.enums.PaymentStatus;
 import com.mealhub.backend.payment.presentation.dto.request.PaymentLogRequest;
 import com.mealhub.backend.payment.presentation.dto.response.PaymentLogResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.UUID;
 
 @RestController
 @RequestMapping("/v1/payment")
