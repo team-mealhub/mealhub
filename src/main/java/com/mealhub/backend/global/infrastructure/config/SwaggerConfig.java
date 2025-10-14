e74266d2fd8d2008b587742c18664fde1d21d78d
rnignon <86004439+rnignon@users.noreply.github.com>
2025-10-14 10:09:13 +0900

    [#56] 로그인 로직 컨트롤러 이전 및 구조 리팩토링 (#57)
    * chore: error.properties 로그인 실패 메세지 키 변경
    
    * refactor: 로그인 로직을 JwtAuthenticationFilter 에서 AuthController로 이전
    
    * docs: Swagger 문서에 Authorize 버튼 추가

--- src/main/java/com/mealhub/backend/global/infrastructure/config/SwaggerConfig.java
+++ src/main/java/com/mealhub/backend/global/infrastructure/config/SwaggerConfig.java
@@ -1,7 +1,10 @@
 package com.mealhub.backend.global.infrastructure.config;
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springdoc.core.models.GroupedOpenApi;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -19,7 +22,21 @@ public class SwaggerConfig {
 
     @Bean
     public OpenAPI customOpenAPI() {
+        String jwtSchemeName = "Authorization";
+
+        SecurityScheme securityScheme = new SecurityScheme()
+                .name(jwtSchemeName)
+                .type(SecurityScheme.Type.HTTP)
+                .scheme("bearer")
+                .bearerFormat("JWT")
+                .in(SecurityScheme.In.HEADER);
+
+        SecurityRequirement securityRequirement = new SecurityRequirement()
+                .addList(jwtSchemeName);
+
         return new OpenAPI()
+                .components(new Components().addSecuritySchemes(jwtSchemeName, securityScheme))
+                .addSecurityItem(securityRequirement)
                 .info(new Info()
                         .version("1.0")
                         .title("Mealhub")
