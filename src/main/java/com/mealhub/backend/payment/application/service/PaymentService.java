ce864f736f52522c31dea842738c137edeac5dc0
rnignon <86004439+rnignon@users.noreply.github.com>
2025-10-14 10:42:58 +0900

    [#8] 결제 내역 도메인 기본 구조 구현 (#54)
    * feat: PaymentLog 엔티티 구현
    
    * feat: PaymentLog 리포지토리 작성
    
    * feat: PaymentLog 관련 Request, Response DTO 작성
    
    * feat: Payment 관련 Service, Controller 코드 구현
    
    * style: 불필요한 import 문 제거
    
    * style: 불필요한 출력문 제거

---
+++ src/main/java/com/mealhub/backend/payment/application/service/PaymentService.java
@@ -0,0 +1,53 @@
+package com.mealhub.backend.payment.application.service;
+
+import com.mealhub.backend.payment.domain.entity.PaymentLog;
+import com.mealhub.backend.payment.domain.entity.QPaymentLog;
+import com.mealhub.backend.payment.domain.repository.PaymentLogRepository;
+import com.mealhub.backend.payment.presentation.dto.request.PaymentLogRequest;
+import com.mealhub.backend.payment.presentation.dto.response.PaymentLogResponse;
+import com.querydsl.core.BooleanBuilder;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class PaymentService {
+
+    private final PaymentLogRepository paymentLogRepository;
+
+    @Transactional
+    public PaymentLogResponse createPaymentLog(PaymentLogRequest.Create request) {
+        PaymentLog paymentLog = PaymentLog.create(request);
+        PaymentLog savedPaymentLog = paymentLogRepository.save(paymentLog);
+        return new PaymentLogResponse(savedPaymentLog);
+    }
+
+    @Transactional(readOnly = true)
+    public Page<PaymentLogResponse> getPaymentLogs(PaymentLogRequest.Search request, int page, int size) {
+        PageRequest pageRequest = PageRequest.of(page, size);
+        Page<PaymentLog> paymentLogs = paymentLogRepository.findAll(buildSearchConditions(request), pageRequest);
+
+        return paymentLogs.map(PaymentLogResponse::new);
+    }
+
+    private BooleanBuilder buildSearchConditions(PaymentLogRequest.Search request) {
+        BooleanBuilder builder = new BooleanBuilder();
+
+        if (request.getUserId() != null) {
+            builder.and(QPaymentLog.paymentLog.userId.eq(request.getUserId()));
+        }
+
+        if (request.getOrderId() != null) {
+            builder.and(QPaymentLog.paymentLog.orderId.eq(request.getOrderId()));
+        }
+
+        if (request.getStatus() != null) {
+            builder.and(QPaymentLog.paymentLog.status.eq(request.getStatus()));
+        }
+
+        return builder;
+    }
+}
---
+++ src/main/java/com/mealhub/backend/payment/presentation/controller/PaymentController.java
@@ -0,0 +1,27 @@
+package com.mealhub.backend.payment.presentation.controller;
+
+import com.mealhub.backend.payment.application.service.PaymentService;
+import com.mealhub.backend.payment.presentation.dto.request.PaymentLogRequest;
+import com.mealhub.backend.payment.presentation.dto.response.PaymentLogResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/v1/payment")
+@RequiredArgsConstructor
+@PreAuthorize("hasRole('MANAGER')")
+public class PaymentController {
+
+    private final PaymentService paymentService;
+
+    @PostMapping("/logs")
+    public Page<PaymentLogResponse> getPaymentLogs(
+            @RequestBody PaymentLogRequest.Search request,
+            @RequestParam(required = false, defaultValue = "0") int page,
+            @RequestParam(required = false, defaultValue = "10") int size
+    ) {
+        return paymentService.getPaymentLogs(request, page, size);
+    }
+}
