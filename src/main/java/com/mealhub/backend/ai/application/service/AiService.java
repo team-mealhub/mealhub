65263ef99abc1904ceee1763e680e04281719452
NaEun Kim <121096778+hellonaeunkim@users.noreply.github.com>
2025-10-15 22:52:06 +0900

    [#9] AI 상품 설명 자동 생성 기능 구현 (#90)
    * chore : Gemini 의존성 추가
    
    * chore : gemini model key 추가
    
    * feat : AI 상품 설명 자동 생성 기능 구현

---
+++ src/main/java/com/mealhub/backend/ai/application/service/AiService.java
@@ -0,0 +1,76 @@
+package com.mealhub.backend.ai.application.service;
+
+import com.google.genai.Client;
+import com.google.genai.types.GenerateContentResponse;
+import com.mealhub.backend.ai.domain.entity.AiEntity;
+import com.mealhub.backend.ai.infrastructure.repository.AiRepository;
+import com.mealhub.backend.ai.presentation.dto.request.AiGenerateDescriptionRequest;
+import com.mealhub.backend.ai.presentation.dto.response.AiGenerateDescriptionResponse;
+import com.mealhub.backend.global.domain.exception.BadRequestException;
+import com.mealhub.backend.restaurant.infrastructure.repository.RestaurantCategoryRepository;
+import com.mealhub.backend.user.domain.entity.User;
+import com.mealhub.backend.user.infrastructure.repository.UserRepository;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class AiService {
+
+    private final UserRepository userRepository;
+    private final RestaurantCategoryRepository restaurantCategoryRepository;
+    private final AiRepository aiRepository;
+
+    @Value("${gemini.api.key}")
+    private String geminiKey;
+
+    @Value("${gemini.model}")
+    private String geminiModel;
+
+    // AI 상품 설명 자동 생성 (OWNER, MANAGER 권한 필요)
+    @Transactional
+    public AiGenerateDescriptionResponse generateDescription(
+            @Valid AiGenerateDescriptionRequest aiGenerateDescriptionRequest, Long userId) {
+
+        // 등록된 유저 확인
+        User findUser = userRepository.findById(userId)
+                .orElseThrow(() -> new BadRequestException("존재하지 않는 유저입니다."));
+
+        // 등록된 카테고리 확인
+        restaurantCategoryRepository.findByCategory(
+                        aiGenerateDescriptionRequest.getProductCategory())
+                .orElseThrow(() -> new BadRequestException("존재하지 않는 카테고리입니다."));
+
+        String aiRequest = "상품 이름: " + aiGenerateDescriptionRequest.getProductName() +
+                ", 상품 카테고리: " + aiGenerateDescriptionRequest.getProductCategory() +
+                "위 상품의 이름과 카테고리를 참고하여 한 문장으로 상품 설명을 작성해줘.";
+
+        // AI 요청 생성
+        GenerateContentResponse response;
+        try (Client client = Client.builder().apiKey(geminiKey).build()) {
+
+            response = client.models.generateContent(
+                    geminiModel,
+                    aiRequest,
+                    null);
+        }
+
+        String aiResponse = response.text();
+
+        // AI 응답 로그
+        log.info("AI 응답: {}, {}", aiResponse, aiRequest);
+
+        // AiEntity 생성
+        AiEntity aiEntity = AiEntity.of(findUser, aiRequest, aiResponse);
+
+        // AiEntity 저장
+        AiEntity save = aiRepository.save(aiEntity);
+
+        return AiGenerateDescriptionResponse.from(aiResponse);
+    }
+}
---
+++ src/main/java/com/mealhub/backend/ai/presentation/controller/AiController.java
@@ -0,0 +1,38 @@
+package com.mealhub.backend.ai.presentation.controller;
+
+import com.mealhub.backend.ai.application.service.AiService;
+import com.mealhub.backend.ai.presentation.dto.request.AiGenerateDescriptionRequest;
+import com.mealhub.backend.ai.presentation.dto.response.AiGenerateDescriptionResponse;
+import com.mealhub.backend.global.infrastructure.config.security.UserDetailsImpl;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+@Tag(name = "Ai Controller", description = "AI 도메인 API")
+@RestController
+@RequestMapping("/v1/ai")
+@RequiredArgsConstructor
+public class AiController {
+
+    private final AiService aiService;
+
+    @PostMapping("/generate-description")
+    @Operation(summary = "AI 상품 설명 자동 생성")
+    @ResponseStatus(HttpStatus.CREATED)
+    public AiGenerateDescriptionResponse generateDescription(
+            @Valid @RequestBody AiGenerateDescriptionRequest aiGenerateDescriptionRequest,
+            @AuthenticationPrincipal UserDetailsImpl userDetailsImpl
+    ) {
+        Long userId = userDetailsImpl.getId();
+
+        return aiService.generateDescription(aiGenerateDescriptionRequest, userId);
+    }
+}
