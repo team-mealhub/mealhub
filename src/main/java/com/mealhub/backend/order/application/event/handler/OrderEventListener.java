6743e13c3c060cfd3db3ee36fd5f3b33478fdb08
rnignon <rnignon@naver.com>
2025-10-15 22:53:19 +0900

    fix: cartItemService의 예외 발생 시 롤백되도록 변경

--- src/main/java/com/mealhub/backend/order/application/event/handler/OrderEventListener.java
+++ src/main/java/com/mealhub/backend/order/application/event/handler/OrderEventListener.java
@@ -7,7 +7,6 @@ import com.mealhub.backend.payment.application.service.PaymentService;
 import com.mealhub.backend.payment.domain.enums.PaymentStatus;
 import com.mealhub.backend.payment.presentation.dto.request.PaymentLogRequest;
 import lombok.RequiredArgsConstructor;
-import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.event.TransactionPhase;
 import org.springframework.transaction.event.TransactionalEventListener;
@@ -16,17 +15,23 @@ import org.springframework.transaction.event.TransactionalEventListener;
 @RequiredArgsConstructor
 public class OrderEventListener {
 
-    private final PaymentService paymentService;
     private final CartItemService cartItemService;
+    private final PaymentService paymentService;
 
-    @Async
-    @TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
+    @TransactionalEventListener(phase = TransactionPhase.BEFORE_COMMIT)
     public void handleOrderCreatedEvent(OrderCreatedEvent event) {
-        PaymentLogRequest.Create paymentLogRequest = createPaymentLogRequest(event);
-        paymentService.createPaymentLog(paymentLogRequest);
-
         CartItemUpdateRequest.Buying cartItemUpdateRequest = createCartItemUpdateRequest(event);
         cartItemService.updateCartItemsBuying(event.getUserId(), cartItemUpdateRequest);
+
+        PaymentLogRequest.Create paymentLogRequest = createPaymentLogRequest(event);
+        paymentService.createPaymentLog(paymentLogRequest);
+    }
+
+    private CartItemUpdateRequest.Buying createCartItemUpdateRequest(OrderCreatedEvent event) {
+        return new CartItemUpdateRequest.Buying(
+                event.getCartItemIds(),
+                true
+        );
     }
 
     private PaymentLogRequest.Create createPaymentLogRequest(OrderCreatedEvent event) {
@@ -35,13 +40,6 @@ public class OrderEventListener {
                 event.getUserId(),
                 event.getAmount(),
                 PaymentStatus.PENDING
-        );
-    }
-
-    private CartItemUpdateRequest.Buying createCartItemUpdateRequest(OrderCreatedEvent event) {
-        return new CartItemUpdateRequest.Buying(
-                event.getCartItemIds(),
-                true
         );
     }
 }
