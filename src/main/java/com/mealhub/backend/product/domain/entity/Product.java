ed5fc894eed67ddb674b29beeceed2a2eb072ee4
rnignon <rnignon@naver.com>
2025-10-15 10:30:31 +0900

    refactor: Product 엔티티의 필드명 변경

--- src/main/java/com/mealhub/backend/product/domain/entity/Product.java
+++ src/main/java/com/mealhub/backend/product/domain/entity/Product.java
@@ -3,7 +3,6 @@ package com.mealhub.backend.product.domain.entity;
 import com.mealhub.backend.global.domain.entity.BaseAuditEntity;
 import com.mealhub.backend.restaurant.domain.entity.RestaurantEntity; // ⭐️ RestaurantEntity import 추가
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -11,59 +10,62 @@ import java.util.UUID;
 
 @Entity(name = "p_product")
 @Getter
-@Builder
+@NoArgsConstructor
 public class Product extends BaseAuditEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
     @Column(name = "p_id", updatable = false, nullable = false)
-    private UUID pId;
+    private UUID id;
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "r_id", nullable = false)
     private RestaurantEntity restaurant;
 
     @Column(name = "p_name", length = 20, nullable = false)
-    private String pName;
+    private String name;
 
     @Column(name = "p_description", length = 255, nullable = true)
-    private String pDescription;
+    private String description;
 
     @Column(name = "p_price", nullable = false)
-    private long pPrice;
+    private long price;
 
     @Column(name = "p_status", nullable = false)
-    private boolean pStatus;
+    private boolean status;
 
-    public static Product createProduct(RestaurantEntity restaurant, String pName, String pDescription, long pPrice, boolean pStatus) {
+    @Builder
+    private Product(RestaurantEntity restaurant, String name, String description, long price, boolean status) {
+        this.restaurant = restaurant;
+        this.name = name;
+        this.description = description;
+        this.price = price;
+        this.status = status;
+    }
+
+    public static Product createProduct(RestaurantEntity restaurant, String name, String description, long price, boolean status) {
         return Product.builder()
-
-                .restaurant(restaurant) // 필드 이름도 'rId'에서 'restaurant'로 변경
-                .pName(pName)
-                .pDescription(pDescription)
-                .pPrice(pPrice)
-                .pStatus(pStatus)
+                .restaurant(restaurant)
+                .name(name)
+                .description(description)
+                .price(price)
+                .status(status)
                 .build();
 
     }
-
 
     /**
      * 음식 정보 수정 (이름, 설명, 가격)
      */
     public void updateInfo(String name, String description, long price) {
         if (name != null && !name.isBlank()) {
-            this.pName = name;
+            this.name = name;
         }
-        this.pDescription = description;
-        this.pPrice = price;
+        this.description = description;
+        this.price = price;
     }
-
-
 
     public void setHidden(boolean isHidden) {
-        this.pStatus = !isHidden;
+        this.status = !isHidden;
     }
-
-
 }
