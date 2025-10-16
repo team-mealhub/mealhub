ef09b178183727a94110d5bba0f0efa9174ddda1
jake8771 <jake525@naver.com>
2025-10-16 23:55:00 +0900

    swagger api 일부분 수정 및 상품 숨김 및 삭제,상세조회 일부 기능 리펙토링

--- src/main/java/com/mealhub/backend/product/infrastructure/repository/ProductRepository.java
+++ src/main/java/com/mealhub/backend/product/infrastructure/repository/ProductRepository.java
@@ -1,19 +1,12 @@
 package com.mealhub.backend.product.infrastructure.repository;
 
 import com.mealhub.backend.product.domain.entity.Product;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.querydsl.QuerydslPredicateExecutor;
-import org.springframework.stereotype.Repository;
-
-import java.util.List;
-import java.util.Optional;
 import java.util.UUID;
-@Repository
+
 public interface ProductRepository extends JpaRepository<Product, UUID>, QuerydslPredicateExecutor<Product> {
 
-    List<Product> findAllByRestaurantRestaurantIdAndStatus(UUID rId,boolean status);
 
 
 }
